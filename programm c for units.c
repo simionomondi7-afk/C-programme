7410a051fd32df2957caa493816f91e1a8e2581f
simionomondi7-afk <simionomondi7@gmail.com>
2025-09-25 20:35:57 +0300

    Add files via upload

---
+++ programm c for units.c
@@ -0,0 +1,23 @@
+/*
+NAME :Simion Omondi Makiri
+REG NO. PA106/G/28766/25
+DESCRIPTION: Simple program displaying units taken */
+
+#include <stdio.h> //preprocessor
+int main(){ //main function
+
+printf("mathematics for science \n");
+printf("Fundamentals of ICT \n");
+printf("introduction to programming \n");
+printf("Descrete mathematics");
+printf("system analysis and design \n");
+printf("system organisation \n");
+printf("fundamental of communication \n");
+
+
+return 0;
+
+
+
+}
+
