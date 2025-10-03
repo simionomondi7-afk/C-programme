9d8a4f08b695f45493f822fc8e0e1b05828bfc11
simionomondi7-afk <simionomondi7@gmail.com>
2025-10-03 14:34:40 +0300

    Add files via upload

---
+++ STUDENT EXAM ELIGIBILITY.c
@@ -0,0 +1,29 @@
+/* NAME: SIMION MAKIRI OMONDI
+   REG NO.: PA106/G/28766/28
+   DESCRIPTION: Simple program to check student eligibility for exams */
+
+
+   #include<stdio.h>
+
+int main(){
+
+         int attendance,average mark;
+    print("enter the student attendance: ");
+         scanf("%d", &attendance);
+
+
+    printf("enter the student average mark: ");
+          scanf("%d", &average mark);
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
